18b66108616170461afaab18e068e9a236e8d309
Franco Berrios <fnberrios@uc.cl>
2020-04-15 00:26:43 -0400

    C5

---
+++ Sites/consultas/consulta_cinco.php
@@ -0,0 +1,45 @@
+<?php include('../templates/header.html');   ?>
+
+<body>
+    <?php
+    #Llama a conexión, crea el objeto PDO y obtiene la variable $mb
+    require("../config/conexion.php");
+
+    #Se obtiene el valor del input del usuario
+    $open = $_POST["hora_apertura"];
+    $cierre = $_POST["hora_cierre"];
+    $city = $_POST["ciudad"];
+
+    #Se construye la consulta como un string
+    $query = "SELECT Lugares.lnombre, Obras.onombre FROM
+    Lugares INNER JOIN LugaresIglesia ON
+    Lugares.lid = LugaresIglesia.lid
+    INNER JOIN Obras ON Obras.lid = Lugares.lid
+    INNER JOIN Ciudades ON Ciudades.cid = Lugares.cid
+    WHERE Ciudades.cnombre = '$city'
+    AND LugaresIglesia.hora_apertura $<=$ '$open' AND
+    LugaresIglesia.hora_cierre
+    $>=$ '$cierre' AND Obras.tipo = 'fresco';";
+
+
+    #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+    $result = $db->prepare($query);
+    $result->execute();
+    $valores = $result->fetchAll();
+    ?>
+
+    <table>
+        <tr>
+            <th>Nombres de las iglesias </th>
+            <th>Nombres de los frescos </th>
+        </tr>
+
+        <?php
+        foreach ($valores as $v) {
+            echo "<tr><td>$v[0]</td><td>$v[1]</td></tr>";
+        }
+        ?>
+
+    </table>
+
+    <?php include('../templates/footer.html'); ?>
--- Sites/index.php
+++ Sites/index.php
@@ -6,6 +6,9 @@
 
   <br>
 
+  <!-------------- CONSULTA 1 ---------------->
+
+
   <h3 align="center"> Todos los Nombres Distintos de las Obras de Arte</h3>
 
   <form align="center" action="consultas/consulta_obras.php" method="post">
@@ -15,6 +18,8 @@
   <br>
   <br>
   <br>
+
+  <!-------------- CONSULTA 2 ---------------->
 
   <h3 align="center"> Muestre todos los nombres de las plazas que contengan
     al menos una escultura de “Gian Lorenzo Bernini" </h3>
@@ -30,6 +35,9 @@
   <br>
   <br>
 
+
+  <!-------------- CONSULTA 3 ---------------->
+
   <h3 align="center"> Ingrese el nombre de un país. Muestre el nombre de todos
     los museos de ese país que tengan obras del renacimiento.</h3>
 
@@ -43,12 +51,37 @@
   <br>
   <br>
 
-
+  <!-------------- CONSULTA 4 ---------------->
 
   <h3 align="center"> Para cada artista, entregue su nombre
-     y el número de obras en las que ha participado. </h3>
+    y el número de obras en las que ha participado. </h3>
 
   <form align="center" action="consultas/consulta_cuatro.php" method="post">
+    <input type="submit" value="Buscar">
+  </form>
+  <br>
+  <br>
+  <br>
+
+
+  <!-------------- CONSULTA 5 ---------------->
+
+  <h3 align="center"> Ingrese una hora de apertura en formatohh:mm:ss,
+    una hora de cierre y una ciudad. Muestre los nombres de las iglesias
+    ubicadas en esa ciudad, abiertas entre esas horas(inclusive) junto a
+    todos los nombres de los frescos que encuentra en cada una de ellas.
+    Una fila por cada tupla.</h3>
+
+  <form align="center" action="consultas/consulta_cinco.php" method="post">
+    Hora de Apertura:
+    <input type="text" name="hora_apertura">
+    <br /><br />
+    Hora de Cierre:
+    <input type="text" name="hora_cierre">
+    <br /><br />
+    Ciudad:
+    <input type="text" name="ciudad">
+    <br /><br />
     <input type="submit" value="Buscar">
   </form>
   <br>
