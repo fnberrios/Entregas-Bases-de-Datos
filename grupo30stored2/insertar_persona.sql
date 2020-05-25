437c66d9e14465958d2b7721f044a6d86269e6dc
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-05-25 18:15:26 -0400

    stored2

---
+++ grupo30stored2/insertar_persona.sql
@@ -0,0 +1,8 @@
+%%sql
+CREATE OR REPLACE FUNCTION insertar_persona (rut varchar, nombre varchar, apellido varchar)
+RETURNS void AS
+$$
+BEGIN
+    INSERT INTO Personas VALUES (rut, nombre, apellido);
+END
+$$ language plpgsql
