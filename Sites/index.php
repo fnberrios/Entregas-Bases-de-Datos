90bf532389339cf94ee8485a75969f29d5fecfc6
Franco Berrios <fnberrios@uc.cl>
2020-05-25 14:45:14 -0400

    CSS

--- Sites/index.php
+++ Sites/index.php
@@ -13,6 +13,15 @@
   <br>
 
   <!-------------- CONSULTA POR ARTISTAS E3 ---------------->
+  <ul>
+    <li><a href="default.html">Home</a></li>
+    <li><a href="news.asp">News</a></li>
+    <li><a href="contact.asp">Contact</a></li>
+    <li><a href="about.asp">About</a></li>
+  </ul>
+
+
+
 
   <div>
     <h3 align="left">Artistas</h3>
