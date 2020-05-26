7b2fe902d4e1f4d96fa4e720262595348d9fa29d
Franco Berrios <fnberrios@uc.cl>
2020-05-26 15:12:12 -0400

    Lugares

--- Sites/consultas/consulta_lugares.php
+++ Sites/consultas/consulta_lugares.php
@@ -64,7 +64,7 @@
         foreach ($dataCollected as $p) {
             echo "<tr> <td><a href='consulta_artistas.php?artista=$p[0]' >$p[0]</td>
                     <td><a href='consulta_obras.php?obra=$p[1]' >$p[1]</td> <td>$p[2]</td>
-                    <td>$p[3]</td></tr>";
+                    <td>$p[3]</td> <td>'Comprar Tickets'</td></tr>";
         }
         ?>
     </table>
