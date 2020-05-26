7a2f178c0795bb75984b2acafd30c8d3f20bb192
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-05-26 02:54:19 -0400

    consulta obras

---
+++ Sites/consultas/consulta_obras.php
@@ -0,0 +1,45 @@
+<?php include('../templates/header.html');   ?>
+
+<body>
+  <?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+  $nom = $_GET["obra"];
+  $query = "SELECT Artista.anombre, Obras.onombre, Creo.fecha_inicio, Creo.fecha_termino,
+  Creo.periodo, Obras.tipo, ObrasPintura.tecnica, ObrasEscultura.material, Lugares.lnombre,
+  Ciudades.cnombre, Ciudades.pais, FROM Obras
+  INNER JOIN Creo ON Creo.oid=Obras.oid
+  INNER JOIN Lugares ON Obras.lid=Lugares.lid
+  INNER JOIN Ciudades ON Ciudades.cid=Lugares.cid
+  INNER JOIN Artistas ON Artistas.aid=Creo.aid
+  LEFT JOIN ObrasPintura ON Creo.oid=ObrasPintura.oid
+  LEFT JOIN ObrasEscultura ON Creo.oid=ObrasEscultura.oid
+  Where Obras.onombre='$nom';";
+  $result = $db->prepare($query);
+  $result->execute();
+  $dataCollected = $result->fetchAll();
+  ?>
+
+  <table>
+    <tr>
+      <th>Nombre del Artista</th>
+      <th>Nombre de la Obra</th>
+      <th>Fecha Inicio</th>
+      <th>Fecha Termino</th>
+      <th>Periodo</th>
+      <th>Tipo</th>
+      <th>Tecnica</th>
+      <th>Material</th>
+      <th>Nombre Lugar</th>
+      <th>Nombre Ciudad</th>
+      <th>Nombre Pais</th>
+    </tr>
+    <?php
+    foreach ($dataCollected as $p) {
+        echo "<tr> <td>$p[0]</td> <td>$p[1]</td> <td>$p[2]</td> <td>$p[3]</td>
+         <td>$p[4]</td> <td>$p[5]</td> <td>$p[6]</td> <td>$p[7]</td> <td>$p[8]</td>
+          <td>$p[9]</td> <td>$p[10]</td></tr>";
+    }
+    ?>
+  </table>
