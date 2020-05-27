178bba570d50c898d1cc1bd3ad334460b811b1e7
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-05-27 19:49:04 -0400

    prueba

--- Sites/consultas/consulta_itinerario.php
+++ Sites/consultas/consulta_itinerario.php
@@ -5,7 +5,7 @@
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../config/conexion.php");
   if(isset($_POST["artistas"]))
-      echo $_POST["artistas"];
+      echo $_POST["artistas"][0];
   ?>
 
   <?php include('../templates/footer.html'); ?>
