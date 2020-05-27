21f5fd40710307029b0addbb13c2ba3f13510621
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-05-27 18:09:00 -0400

    prueba

--- Sites/index.php
+++ Sites/index.php
@@ -47,7 +47,7 @@
   <ul>
     <?php
     foreach ($valores as $v) {
-      echo "<option><a href='consultas/consulta_artistas.php?artista=$v[0]' >$v[0]</a></option>";
+      echo "<li><a href='consultas/consulta_artistas.php?artista=$v[0]' >$v[0]</a></li>";
       # echo "<li><input type="button" style="display:inline" value=$v[0]/></li>";
       # echo "<input type="submit" value="$v[0]">";
     }
