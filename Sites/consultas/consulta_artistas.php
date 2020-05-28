f11994eea4ba302475b360300b348cfff88fc778
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-05-27 20:42:56 -0400

    prueba

--- Sites/consultas/consulta_artistas.php
+++ Sites/consultas/consulta_artistas.php
@@ -5,7 +5,7 @@
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../config/conexion.php");
 
-  $nom = $_POST["artista"]
+  $nom = $_POST["artista"];
   $query = "SELECT Artistas.anombre, Artistas.nacimiento, Artistas.descripcion,
   ArtistasFallecidos.fallecimiento FROM Artistas LEFT JOIN ArtistasFallecidos ON
   Artistas.aid=ArtistasFallecidos.aid WHERE Artistas.aid ='$nom';";
