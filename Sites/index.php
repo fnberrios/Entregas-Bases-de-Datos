dbf51c8141fe0d30af3294ca4315f1615f15eb6f
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-05-29 01:56:10 -0400

    prueba

--- Sites/index.php
+++ Sites/index.php
@@ -65,7 +65,7 @@
   <h3 align="center">¡Cliquea algún país en el mapa!</h3>
   <img src="img/mapa.jpeg" usemap="#mapa">
   <map name="mapa">
-  <area shape="circle" coords="136,287,70" href="consultas/consulta_cuatro.php">
+  <area shape="circle" coords="136,287,70" href="consultas/consulta_cuatro.php?pais='francia'">
   <area shape="circle" coords="75,125,50" href="consultas/consulta_cuatro.php">
   <area shape="circle" coords="266,161,60" href="consultas/consulta_cuatro.php">
   <area shape="circle" coords="307,371,50" href="consultas/consulta_cuatro.php">
