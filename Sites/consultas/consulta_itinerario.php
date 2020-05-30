4262e8c2645ea05db5e9f88dba9c8cee1d786b55
Franco Berrios <fnberrios@uc.cl>
2020-05-30 16:15:55 -0400

    PA

--- Sites/consultas/consulta_itinerario.php
+++ Sites/consultas/consulta_itinerario.php
@@ -5,17 +5,52 @@
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../config/conexion.php");
   if (isset($_POST["artistas"]))
-    $artistas = $_POST["artistas"];
-  $query = "ARRAY(SELECT * FROM Artistas INNER JOIN unnest($artistas) lista ON 
-      Artistas.aid=lista);";
-  $result = $db30->prepare($query);
-  $result->execute();
-  $lista_artistas = $result->fetchAll();
-
+    $lista_artistas = $_POST["artistas"];
   $fecha_inicio = $_POST["fecha-inicio"];
   $ciudad_origen = $_POST["nciudad"];
 
   $query = "SELECT * FROM itinerario_dos_ciudades($lista_artistas, $ciudad_origen);";
+  $result = $db30->prepare($query);
+  $result->execute();
+  $itinerario_dos_ciudades = $result->fetchAll();
+  ?>
+  <table>
+    <tr>
+      <th>Listado de Ciudades</th>
+      <th>Artistas que puedes ver</th>
+    </tr>
+    <?php
+    foreach ($itinerario_dos_ciudades as $p) {
+      echo "<tr> <td>$p[0]</td> <td>$p[1]</td></tr>";
+    }
+    ?>
+  </table>
+
+
+
+  <?php
+  $query = "SELECT * FROM itinerario_tres_ciudades($lista_artistas, $ciudad_origen);";
+  $result = $db30->prepare($query);
+  $result->execute();
+  $itinerario_dos_ciudades = $result->fetchAll();
+  ?>
+  <table>
+    <tr>
+      <th>Listado de Ciudades</th>
+      <th>Artistas que puedes ver</th>
+    </tr>
+    <?php
+    foreach ($itinerario_dos_ciudades as $p) {
+      echo "<tr> <td>$p[0]</td> <td>$p[1]</td></tr>";
+    }
+    ?>
+  </table>
+
+
+
+  <?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  $query = "SELECT * FROM itinerario_cuatro_ciudades($lista_artistas, $ciudad_origen);";
   $result = $db30->prepare($query);
   $result->execute();
   $itinerario_dos_ciudades = $result->fetchAll();
