8f516f66c5f4b799a03f7f4f4eb3fab8d7d6e0c9
Franco Berrios <fnberrios@uc.cl>
2020-05-30 17:27:35 -0400

    PAAAAAAAAAAA

--- Sites/consultas/consulta_itinerario.php
+++ Sites/consultas/consulta_itinerario.php
@@ -21,7 +21,8 @@
   echo gettype($lista_artistas);
   echo gettype($lista_artistas[0]);
   echo gettype($c_origen);
-  $query = "SELECT * FROM itinerario_dos_ciudades($lista_artistas, '$c_origen');";
+  $lista_artistas_sql = "{" + implode(",", $lista_artistas) + "}";
+  $query = "SELECT * FROM itinerario_dos_ciudades($lista_artistas_sql, '$c_origen');";
   $result = $db30->prepare($query);
   $result->execute();
   $itinerario_dos_ciudades = $result->fetchAll();
