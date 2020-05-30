ec986f9ec07fc15799f481b49388a046874f8432
Franco Berrios <fnberrios@uc.cl>
2020-05-30 15:58:01 -0400

    Procedimiento Almacenado

--- Sites/consultas/consulta_itinerario.php
+++ Sites/consultas/consulta_itinerario.php
@@ -4,12 +4,28 @@
   <?php
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../config/conexion.php");
-  if(isset($_POST["artistas"]))
-    echo $_POST["artistas"];
+  if (isset($_POST["artistas"]))
+    $lista_artistas = $_POST["artistas"];
   $fecha_inicio = $_POST["fecha-inicio"];
-  echo $fecha_inicio;
-  $ciudad = $_POST["nciudad"];
-  echo $ciudad;
+  $ciudad_origen = $_POST["nciudad"];
+
+  $query = "SELECT * FROM itinerario_dos_ciudades($lista_artistas, $ciudad_origen);";
+  $result = $db30->prepare($query);
+  $result->execute();
+  $itinerario_dos_ciudades = $result->fetchAll();
   ?>
+  <table>
+    <tr>
+      <th>Listado de Ciudades</th>
+      <th>Artistas que puedes ver</th>
+    </tr>
+    <?php
+    foreach ($itinerario_dos_ciudades as $p) {
+      echo "<tr> <td>$p[0]</td> <td>$p[1]</td></tr>";
+    }
+    ?>
+  </table>
+
+
 
   <?php include('../templates/footer.html'); ?>
