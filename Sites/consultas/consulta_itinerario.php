108a3a73c48785b6d11390cf5e799d450c8cf16a
Franco Berrios <fnberrios@uc.cl>
2020-05-31 00:11:25 -0400

    Weaita version 5463729294857362728

--- Sites/consultas/consulta_itinerario.php
+++ Sites/consultas/consulta_itinerario.php
@@ -6,11 +6,11 @@
   require("../config/conexion.php");
   if (isset($_POST["artistas"]))
     $lista_artistas_2 = $_POST["artistas"];
-    $lista_artistas=ARRAY();
-    foreach ($lista_artistas_2 as $artista){
-      $nom = str_replace('-', ' ', $artista);
-      array_push($lista_artistas, $nom);
-    }
+  $lista_artistas = array();
+  foreach ($lista_artistas_2 as $artista) {
+    $nom = str_replace('-', ' ', $artista);
+    array_push($lista_artistas, $nom);
+  }
   $fecha_inicio = $_POST["fecha-inicio"];
   $c_origen = $_POST["nciudad"];
   $lista_artistas_sql = implode(',', $lista_artistas);
@@ -34,15 +34,39 @@
 
 
   <?php
-  $query = "SELECT * FROM itinerario_tres_ciudades(string_to_array('$lista_artistas_sql',','), '$c_origen');";
+  $query = "SELECT * FROM info_itinerario_tres_ciudades(string_to_array('$lista_artistas_sql',','), '$c_origen');";
+  $result = $db30->prepare($query);
+  $result->execute();
+  $query = "SELECT * FROM medios_precios;";
+  $result = $db30->prepare($query);
+  $result->execute();
+  $medios_precios = $result->fetchAll();
+  $query = "SELECT id_itinerario, SUM(precio) AS total FROM medios_precios GROUP BY id_itinerario ORDER BY total;";
   $result = $db30->prepare($query);
   $result->execute();
   $itinerario_tres_ciudades = $result->fetchAll();
   ?>
   <table>
     <tr>
-      <th>Listado de Ciudades</th>
-      <th>Artistas que puedes ver</th>
+      <th>ID_Itinerario</th>
+      <th>Ciudad inicial</th>
+      <th>Ciudad posterior</th>
+      <th>Medio</th>
+      <th>Precio</th>
+      <th>Duracion</th>
+      <th>Hora de Salida</th>
+    </tr>
+    <?php
+    foreach ($medios_precios as $p) {
+      echo "<tr> <td>$p[0]</td> <td>$p[1]</td> <td>$p[2]</td> <td>$p[3]</td> <td>$p[4]</td> <td>$p[5]</td> <td>$p[6]</td></tr>";
+    }
+    ?>
+  </table>
+
+  <table>
+    <tr>
+      <th>ID_Itinerario</th>
+      <th>Precio TotalArtistas que puedes ver</th>
     </tr>
     <?php
     foreach ($itinerario_tres_ciudades as $p) {
