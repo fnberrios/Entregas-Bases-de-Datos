da86fc96f542b0d76cb2d066c4bfc7423337104c
TrigalJeruti <tjesquivel@uc.cl>
2020-06-01 18:39:18 -0400

    Subiendo avance

--- Sites/consultas/consulta_reserva.php
+++ Sites/consultas/consulta_reserva.php
@@ -1,6 +1,8 @@
 <?php
   session_start();
-  include('../templates/header.html');   
+?>
+<?php
+  include('../templates/header.html');
 
   require("../config/conexion.php");
   $fechainicio = $_POST["fi"];
@@ -23,15 +25,15 @@
     $rid_actual = $r[0];
   echo "$rid_actual";
 
-  $sql1 = "INSERT INTO reservas (rid, hid, fechainicio, fechatermino) VALUES ('$rid_actual','$hotel','$fechainicio','$fechatermino')";
-  $result2 = $db53 -> prepare($sql1);
-  $result2 -> execute();
-  $dataCollected2 = $result2 -> fetchAll();
-
-  $sql2 = "INSERT INTO realiza (rid, uid) VALUES ('$rid_actual', '$usuario_actual')";
-  $result3 = $db53 -> prepare($sql2);
-  $result3 -> execute();
-  $dataCollected3 = $result3 -> fetchAll();
+  // $sql1_ = "INSERT INTO reservas (rid, hid, fechainicio, fechatermino) VALUES ('$rid_actual','$hotel','$fechainicio','$fechatermino')";
+  // $result2 = $db53 -> prepare($sql1_);
+  // $result2 -> execute();
+  // $dataCollected2 = $result2 -> fetchAll();
+  //
+  // $sql2 = "INSERT INTO realiza (rid, uid) VALUES ('$rid_actual', '$usuario_actual')";
+  // $result3 = $db53 -> prepare($sql2);
+  // $result3 -> execute();
+  // $dataCollected3 = $result3 -> fetchAll();
 
   // header('Location: consulta_hoteles.php');
 ?>
