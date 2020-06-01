f80a34b371b1a779a1e5374dd688b871f4a16bbd
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-06-01 17:22:27 -0400

    prueba

--- Sites/index.php
+++ Sites/index.php
@@ -82,6 +82,7 @@
 
   <?php
   else:?>
+
     <!-------------- INGRESAR E3 ---------------->
     <h3 align="center"> Ingresa a tu cuenta</h3>
     <form align="center" action="consultas/consulta_form_ingresar.php" method="post">
@@ -95,9 +96,6 @@
       <input type="submit" value="Registrarme">
     </form>
   <?php endif;?>
-
-
-
 
 
 
@@ -122,6 +120,16 @@
   $result = $db53->prepare($query);
   $result->execute();
   $ciudades = $result->fetchAll();
+  ?>
+  <?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $mb
+  require("config/conexion.php");
+  #Se construye la consulta como un string
+  $query = " SELECT anombre, aid FROM Artistas;";
+  #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+  $result = $db30->prepare($query);
+  $result->execute();
+  $valores = $result->fetchAll();
   ?>
 
   <h2 align="center">¡Crea tu itinerario para visitar las obras de tus artistas favoritos!</h2>
@@ -151,102 +159,6 @@
   <br /><br />
     <input type="submit" value="Enviar">
   </form>
-  <!-------------- CONSULTA 1 ---------------->
-
-
-  <h3 align="center">¿Quieres saber todos los nombres distintos de las obras?</h3>
-
-  <form align="center" action="consultas/consulta_uno.php" method="post">
-    <input type="submit" value="Buscar">
-  </form>
-
-  <br>
-  <br>
-  <br>
-
-  <!-------------- CONSULTA 2 ---------------->
-
-  <h3 align="center"> ¿Quieres saber todos los nombres de las plazas que contengan
-    al menos una escultura de “Gian Lorenzo Bernini"? </h3>
-
-  <form align="center" action="consultas/consulta_dos.php" method="post">
-    <!-- Id:
-    <input type="text" name="id_elegido">
-    <br /><br /> -->
-    <input type="submit" value="Buscar">
-  </form>
-
-  <br>
-  <br>
-  <br>
-
-
-  <!-------------- CONSULTA 3 ---------------->
-
-    <h3 align="center"> ¿Quieres conocer el nombre de todos
-      los museos de un país que tengan obras del renacimiento?</h3>
-
-    <form align="center" action="consultas/consulta_tres.php" method="post">
-      País:
-      <input type="text" name="npais">
-      <br /><br />
-      <input type="submit" value="Buscar">
-    </form>
-    <br>
-    <br>
-    <br>
-
-
-  <!-------------- CONSULTA 4 ---------------->
-
-  <h3 align="center"> ¿Quieres saber, para cada artista, su nombre
-    y el número de obras en las que ha participado? </h3>
-
-  <form align="center" action="consultas/consulta_cuatro.php" method="post">
-    <input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-  <br>
-
-
-  <!-------------- CONSULTA 5 ---------------->
-
-  <h3 align="center"> ¿Quieres conocer los nombres de las iglesias
-    ubicadas en una ciudad,</h3>
-  <h3 align="center"> abiertas entre algunas horas(inclusive), junto a
-    todos los nombres de los frescos que se encuentra en cada una de ellas?
-  </h3>
-
-  <form align="center" action="consultas/consulta_cinco.php" method="post">
-    Hora de Apertura (Formato XX:XX:XX):
-    <input type="text" name="hora_apertura">
-    <br /><br />
-    Hora de Cierre (Formato XX:XX:XX):
-    <input type="text" name="hora_cierre">
-    <br /><br />
-    Ciudad:
-    <input type="text" name="ciudad">
-    <br /><br />
-    <input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-  <br>
-
-  <!-------------- CONSULTA 6 ---------------->
-
-  <h3 align="center">¿Quieres saber el nombre de cada museo, plaza o
-    iglesia que tenga obras de todos los periodos del arte que existan
-    en la base de datos?</h3>
-
-  <form align="center" action="consultas/consulta_seis.php" method="post">
-    <br /><br />
-    <input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-  <br>
 
   <br>
 </body>
