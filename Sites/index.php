42afcf2f2349a7abc6d069b96142a1fcd4d7c0dd
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-06-01 17:27:27 -0400

    prueba

--- Sites/index.php
+++ Sites/index.php
@@ -8,11 +8,6 @@
 <?php include('templates/header.html');   ?>
 
 
-<a href="google.com"><img alt="" src="https://lh3.googleusercontent.com/-IhVc_Wxy6dY/AAAAAAAAAAI/AAAAAAAAAAA/fl45Fty4PEI/photo.jpg" style="width: 154px; height: 47px;" /></a>
-
-<img src="w3schools.jpg" alt="W3Schools.com" width="104" height="142">
-
-
 <body>
   <h1 align="center">AAAgencia de Viajes Splinter S.A.</h1>
   <p style="text-align:center;">¡Aquí podrás encontrar información sobre obras de arte!</p>
