cf595995ab045ef80c814f935d8f1f8d56eea216
Franco Berrios <fnberrios@uc.cl>
2020-06-01 00:50:34 -0400

    PA Entrega3

---
+++ Entrega3/Procedimientos Almacenados/info_itinerario_tres_ciudades.sql
@@ -0,0 +1,75 @@
+%%sql
+
+CREATE OR REPLACE FUNCTION
+info_itinerario_tres_ciudades
+(lista_artistas varchar[], ciudad_origen varchar)
+RETURNS VOID AS $$
+DECLARE
+    tupla varchar[];
+    tupla1 RECORD;
+    tupla2 RECORD;
+    id_variable integer;
+BEGIN
+    DROP TABLE IF EXISTS medios_precios;
+    DROP TABLE IF EXISTS info_itinerarios;
+    CREATE TABLE medios_precios
+    (
+        id_itinerario integer,
+        ciudad1 VARCHAR,
+        ciudad2 VARCHAR,
+        medio VARCHAR,
+        precio integer,
+        duracion integer,
+        hora_salida time
+    );
+    CREATE TABLE info_itinerarios
+    (
+        id_itinerario integer,
+        precio_total integer
+    );
+    id_variable := 1;
+
+FOR tupla IN
+SELECT DISTINCT ciudades
+FROM itinerario_tres_ciudades(lista_artistas, ciudad_origen) LOOP
+
+FOR tupla1 IN
+SELECT t1.ciudad_origen, t1.nombreciudad, t1.medio, t1.precio, t1.duracion, t1.hora_salida
+FROM
+    dblink('dbname=grupo53e3 host=localhost user=grupo53 password=grupo53',
+    'SELECT ciudad_origen, nombreciudad, medio, precio, duracion, hora_salida FROM Destinos INNER JOIN Viaje ON Destinos.did=Viaje.did INNER
+    JOIN Ciudades ON Ciudades.cid=Viaje.cid') AS 
+    t1(ciudad_origen
+varchar, nombreciudad varchar, medio varchar, precio integer, duracion integer, hora_salida time) 
+    WHERE t1.ciudad_origen=tupla[1] AND t1.nombreciudad=tupla[2] LOOP
+
+    
+    FOR tupla2 IN
+SELECT t2.ciudad_origen, t2.nombreciudad, t2.medio, t2.precio, t2.duracion, t2.hora_salida
+FROM
+    dblink('dbname=grupo53e3 host=localhost user=grupo53 password=grupo53',
+      'SELECT ciudad_origen, nombreciudad, medio, precio, duracion, hora_salida FROM Destinos INNER JOIN Viaje ON Destinos.did=Viaje.did INNER
+      JOIN Ciudades ON Ciudades.cid=Viaje.cid')
+      AS t2(ciudad_origen
+varchar, nombreciudad varchar, medio varchar, precio integer, duracion integer, hora_salida time) 
+      WHERE t2.ciudad_origen=tupla[2] AND t2.nombreciudad=tupla[3] LOOP
+
+INSERT INTO medios_precios
+VALUES
+    (id_variable, tupla1.ciudad_origen, tupla1.nombreciudad,
+        tupla1.medio, tupla1.precio, tupla1.duracion, tupla1.hora_salida);
+
+INSERT INTO medios_precios
+VALUES
+    (id_variable, tupla2.ciudad_origen, tupla2.nombreciudad,
+        tupla2.medio, tupla2.precio, tupla2.duracion, tupla2.hora_salida);
+id_variable := id_variable + 1;
+
+
+END LOOP;
+    id_variable := id_variable + 1;
+END LOOP;
+END LOOP;
+
+END;
+$$ language plpgsql
---
+++ Entrega3/Procedimientos Almacenados/itinerario_cuatro_ciudades.sql
@@ -0,0 +1,49 @@
+%%sql
+
+CREATE OR REPLACE FUNCTION
+itinerario_cuatro_ciudades
+(lista_artistas varchar[], ciudad_origen varchar)
+RETURNS TABLE
+(ciudades VARCHAR[], artistas VARCHAR[]) AS $$
+DECLARE
+    combi_ciudades varchar[];
+    posibles_artistas varchar[];
+    resultado varchar[];
+    tupla RECORD;
+BEGIN
+    DROP TABLE IF EXISTS itinerario_ciudades;
+    CREATE TABLE itinerario_ciudades
+    (
+        ciudades VARCHAR
+        [], artistas VARCHAR[]);
+
+    FOR tupla IN
+    SELECT *
+    FROM dos_escalas(ciudad_origen) LOOP
+    combi_ciudades := ARRAY [tupla.origen, tupla.ciudad1, tupla.ciudad2, tupla.final];
+posibles_artistas := ARRAY
+    (SELECT DISTINCT Artistas.anombre
+    FROM Artistas
+        INNER JOIN Creo ON Artistas.aid=Creo.aid INNER JOIN Obras ON Obras.oid=Creo.oid
+        INNER JOIN Lugares ON Obras.lid=Lugares.lid INNER JOIN Ciudades ON Ciudades.cid=Lugares.cid
+        INNER JOIN unnest(lista_artistas) lista ON 
+      Artistas.anombre=lista INNER JOIN unnest(combi_ciudades) lista2 ON Ciudades.cnombre=lista2);
+    INSERT INTO itinerario_ciudades
+    VALUES
+        (combi_ciudades, posibles_artistas);
+    END LOOP;
+    RETURN QUERY
+    SELECT *
+    FROM itinerario_ciudades
+    WHERE 
+ARRAY(
+    SELECT unnest(itinerario_ciudades.artistas)
+    ORDER BY 1
+    )=
+ARRAY
+    (SELECT unnest(lista_artistas)
+    ORDER BY 1
+    );
+    RETURN;
+    END;
+$$ language plpgsql
