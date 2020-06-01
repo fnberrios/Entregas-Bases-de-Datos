5154f5853495339d9268b04b7c52b7fc14c60605
TrigalJeruti <tjesquivel@uc.cl>
2020-06-01 04:28:42 -0400

    Subiendo avance

--- Sites/consultas/consulta_comprar_entrada.php
+++ Sites/consultas/consulta_comprar_entrada.php
@@ -1,19 +1,21 @@
 <?php
   session_start();
-
-
-require("../config/conexion.php");
-
-$lugar = $_GET["lugar"];
-echo "$lugar";
-$date = date('Y-m-d');
-$query1 = "INSERT INTO entradas (uid, lid, fecha_actual) VALUES (:uid,'$lugar','$date');";
-$result1 = $db30 -> prepare($query1);
-$result1 -> bindParam(':uid', $_SESSION['user_id']); #se relacionan
-$result1 -> execute();
-
 ?>
 <?php include('../templates/header.html');   ?>
+<body>
+  <?php
+  require("../config/conexion.php");
+
+  $lugar = $_GET["lugar"];
+  echo "$lugar";
+  $date = date('Y-m-d');
+  $query1 = "INSERT INTO entradas (uid, lid, fecha_actual) VALUES (:uid,'$lugar','$date');";
+  $result1 = $db30 -> prepare($query1);
+  $result1 -> bindParam(':uid', $_SESSION['user_id']); #se relacionan
+  $result1 -> execute();
+
+  ?>
+
 
 <p> Su compra se ha realizado con exito </p>
 
