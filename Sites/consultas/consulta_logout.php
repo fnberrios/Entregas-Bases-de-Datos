14683da5d32913be5fb17cb449330dded8c987bd
TrigalJeruti <tjesquivel@uc.cl>
2020-06-01 15:35:57 -0400

    Subiendo avance

--- Sites/consultas/consulta_logout.php
+++ Sites/consultas/consulta_logout.php
@@ -5,16 +5,18 @@
 <?php
   if (isset($_SESSION['eliminado'])):?>
     	<h3>Su cuenta a sido eliminada exitosamente.</h3>
-
+      <?php
+        session_unset();
+        session_destroy();
+      ?>
+      <form align="center" action="../index.php" method="post">
+        <input type="submit" value="Ir al Inicio">
+      </form>
+<?php
+  else:
+    session_unset();
+    session_destroy();
+    header('Location: ../index.php ');
+?>
 <?php
   endif;?>
-
-<?php
-  session_unset();
-  session_destroy();
-  echo "Termineee";
-  // header('Location: ../index.php ');
-?>
-<form align="center" action="../index.php" method="post">
-  <input type="submit" value="Ir al Inicio">
-</form>
