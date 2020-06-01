a3ff136db1f08a2b27653052d9ba8adfca294c0a
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-06-01 00:17:47 -0400

    prueba

--- Sites/consultas/consulta_itinerario.php
+++ Sites/consultas/consulta_itinerario.php
@@ -16,15 +16,37 @@
   $lista_artistas_sql = implode(',', $lista_artistas);
   echo $lista_artistas_sql;
   echo $c_origen;
-  $query = "SELECT * FROM itinerario_dos_ciudades(string_to_array('$lista_artistas_sql',','), '$c_origen');";
+
+  $query = "SELECT * FROM info_itinerario_dos_ciudades(string_to_array('$lista_artistas_sql',','), '$c_origen');";
+  $result = $db30->prepare($query);
+  $result->execute();
+  $medios_precios = $result->fetchAll();
+  $query = "SELECT id_itinerario, SUM(precio) AS total FROM medios_precios GROUP BY id_itinerario ORDER BY total;";
   $result = $db30->prepare($query);
   $result->execute();
   $itinerario_dos_ciudades = $result->fetchAll();
   ?>
   <table>
     <tr>
-      <th>Listado de Ciudades</th>
-      <th>Artistas que puedes ver</th>
+      <th>ID_Itinerario</th>
+      <th>Ciudad inicial</th>
+      <th>Ciudad posterior</th>
+      <th>Medio</th>
+      <th>Precio</th>
+      <th>Duracion</th>
+      <th>Hora de Salida</th>
+    </tr>
+    <?php
+    foreach ($medios_precios as $p) {
+      echo "<tr> <td>$p[0]</td> <td>$p[1]</td> <td>$p[2]</td> <td>$p[3]</td> <td>$p[4]</td> <td>$p[5]</td> <td>$p[6]</td></tr>";
+    }
+    ?>
+  </table>
+
+  <table>
+    <tr>
+      <th>ID_Itinerario</th>
+      <th>Precio TotalArtistas que puedes ver</th>
     </tr>
     <?php
     foreach ($itinerario_dos_ciudades as $p) {
@@ -32,7 +54,6 @@
     }
     ?>
   </table>
-
 
 
   <?php
@@ -78,19 +99,41 @@
     ?>
   </table>
 
-
-
   <?php
-  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-  $query = "SELECT * FROM itinerario_cuatro_ciudades(string_to_array('$lista_artistas_sql',','), '$c_origen');";
+  $query = "SELECT * FROM info_itinerario_cuatro_ciudades(string_to_array('$lista_artistas_sql',','), '$c_origen');";
+  $result = $db30->prepare($query);
+  $result->execute();
+  $correr_funcion = $result->fetchAll();
+  $query = "SELECT * FROM medios_precios;";
+  $result = $db30->prepare($query);
+  $result->execute();
+  $medios_precios = $result->fetchAll();
+  $query = "SELECT id_itinerario, SUM(precio) AS total FROM medios_precios GROUP BY id_itinerario ORDER BY total;";
   $result = $db30->prepare($query);
   $result->execute();
   $itinerario_cuatro_ciudades = $result->fetchAll();
   ?>
   <table>
     <tr>
-      <th>Listado de Ciudades</th>
-      <th>Artistas que puedes ver</th>
+      <th>ID_Itinerario</th>
+      <th>Ciudad inicial</th>
+      <th>Ciudad posterior</th>
+      <th>Medio</th>
+      <th>Precio</th>
+      <th>Duracion</th>
+      <th>Hora de Salida</th>
+    </tr>
+    <?php
+    foreach ($medios_precios as $p) {
+      echo "<tr> <td>$p[0]</td> <td>$p[1]</td> <td>$p[2]</td> <td>$p[3]</td> <td>$p[4]</td> <td>$p[5]</td> <td>$p[6]</td></tr>";
+    }
+    ?>
+  </table>
+
+  <table>
+    <tr>
+      <th>ID_Itinerario</th>
+      <th>Precio TotalArtistas que puedes ver</th>
     </tr>
     <?php
     foreach ($itinerario_cuatro_ciudades as $p) {
@@ -98,6 +141,10 @@
     }
     ?>
   </table>
+
+
+  <?php
+
 
 
 
