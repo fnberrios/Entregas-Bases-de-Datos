4649b63c19958a7c90e44d701fa6ee0d295bdd34
TrigalJeruti <tjesquivel@uc.cl>
2020-05-31 21:41:45 -0400

    Subiendo avance

--- Sites/consultas/consulta_perfil.php
+++ Sites/consultas/consulta_perfil.php
@@ -16,11 +16,17 @@
       $result -> execute();
       $users_ = $result -> fetchAll();
 
-      $query1 = "SELECT reservas.fechainicio, reservas.fechatermino, hoteles.direccionhotel  FROM realiza, reservas, hoteles WHERE realiza.uid = :uid AND realiza.rid = reservas.rid AND reservas.hid = hoteles.hid;";
+      $query1 = "SELECT reservas.fechainicio, reservas.fechatermino, hoteles.direccionhotel  FROM realiza, reservas,  hoteles WHERE realiza.uid = :uid AND realiza.rid = reservas.rid AND reservas.hid = hoteles.hid;";
       $result1 = $db53 -> prepare($query1);
-      $result1->bindParam(':uid', $_SESSION['user_id']);
+      $result1->bindParam(':uid', $_SESSION['user_id']); #se relacionan
       $result1 -> execute();
       $reservas = $result1 -> fetchAll();
+
+      $query2 = "SELECT tickets.asiento, tickets.fechacompra, tickets.fechaviaje, destinos.ciudad_origen, ciudades.nombreciudad FROM tickets,destinos,viaje,ciudades WHERE tickets.uid = :uid AND tickets.did = destinos.did AND viaje.did = destinos.did AND viaje.cid = ciudades.cid; ";
+      $result2 = $db53 -> prepare($query2);
+      $result2->bindParam(':uid', $_SESSION['user_id']); #se relacionan
+      $result2 -> execute();
+      $tickets = $result2 -> fetchAll();
     }
 
   ?>
@@ -49,6 +55,21 @@
       <?php
       foreach ($reservas as $r) {
         echo "<tr><td>$r[0]</td><td>$r[1]</td><td>$r[2]</td></tr>";
+      }
+      ?>
+    </table>
+    </br>
+    <table>
+      <tr>
+        <th>n° asiento</th>
+        <th>Fecha de inicio</th>
+        <th>Fecha de termino</th>
+        <th>Ciudad de compra</th>
+        <th>Ciudad del viaje</th>
+      </tr>
+      <?php
+      foreach ($tickets as $t){
+        echo "<tr><td>$t[0]</td><td>$t[1]</td><td>$t[2]</td><td>$t[3]</td><td>$t[4]</td></tr>";
       }
       ?>
     </table>
