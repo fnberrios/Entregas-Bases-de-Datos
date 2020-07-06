c2a4159c8fd7be5efccca61f71c71bf58f437c71
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-07-06 17:50:22 -0400

    a

--- Sites/consultas/consulta_uno_api.php
+++ Sites/consultas/consulta_uno_api.php
@@ -2,38 +2,17 @@
 
 <body>
     <?php
-    #Llama a conexión, crea el objeto PDO y obtiene la variable $mb
-    require("../config/conexion.php");
+    require("../config/conexion.php"); #Llama a conexión, crea el objeto PDO y obtiene la variable $db
 
-    #Se obtiene el valor del input del usuario
-    $pais = $_POST["npais"];
-
-    #Se construye la consulta como un string
-    $query = "SELECT DISTINCT lnombre FROM Lugares
-    JOIN Ciudades ON Ciudades.cid = Lugares.cid
-    JOIN Obras ON Obras.lid = Lugares.lid
-    JOIN Creo ON Creo.oid = Obras.oid
-    WHERE Lugares.tipo = 'museo' AND Creo.periodo = 'Renacimiento'
-    AND upper(Ciudades.cpais) LIKE upper('%$pais%');";
-
-
-    #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
-    $result = $db->prepare($query);
-    $result->execute();
-    $museos = $result->fetchAll();
+    $query = "SELECT * FROM usuarios;";
+    $result = $db53 -> prepare($query);
+    $result -> execute();
+    $dataCollected = $result -> fetchAll();
+    foreach ($dataCollected as $p) {
+        echo "<tr> <td>$p[0]</td> <td>$p[1]</td> <td>$p[2]</td> <td>$p[3]</td>
+        <td>$p[4]</td> <td>$p[5]</td> <td>$p[6]</td>
+        <td>$p[7]</td> <td>$p[8]</td>
+        <td>$p[9]</td></tr>";
+    }
     ?>
-
-    <table>
-        <tr>
-            <th>Nombres de los museos </th>
-        </tr>
-
-        <?php
-        foreach ($museos as $m) {
-            echo "<tr><td>$m[0]</td></tr>";
-        }
-        ?>
-
-    </table>
-
     <?php include('../templates/footer.html'); ?>
