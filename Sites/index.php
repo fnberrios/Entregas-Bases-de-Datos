71702573cf3df0e5277b6cdec21c70d937798832
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-07-06 17:54:13 -0400

    a

--- Sites/index.php
+++ Sites/index.php
@@ -15,45 +15,20 @@
   <br>
 
   <?php
-  function CallAPI($method, $url, $data = false)
-  {
-      $curl = curl_init();
-
-      switch ($method)
-      {
-          case "POST":
-              curl_setopt($curl, CURLOPT_POST, 1);
-
-              if ($data)
-                  curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-              break;
-          case "PUT":
-              curl_setopt($curl, CURLOPT_PUT, 1);
-              break;
-          default:
-              if ($data)
-                  $url = sprintf("%s?%s", $url, http_build_query($data));
-      }
-
-      // Optional Authentication:
-      curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-      curl_setopt($curl, CURLOPT_USERPWD, "username:password");
-
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-      $result = curl_exec($curl);
-
-      curl_close($curl);
-
-      return $result;
-  }
-  $data= array('id1'=>41,
-              'id2'=>1);
-  $query =  CallApi($GET, 'https://e5db.herokuapp.com/messages',$data);
-  echo $query;
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $mb
+  require("config/conexion.php");
+  #Se construye la consulta como un string
+  $query = " SELECT * FROM usuarios;";
+  #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+  $result = $db53->prepare($query);
+  $result->execute();
+  $valores = $result->fetchAll();
   ?>
-
+  <?php
+  foreach ($valores as $v) {
+    echo "<option value=$v[1]>$v[0]</option>";
+  }
+  ?>
   <!-------------- CONSULTA POR ARTISTAS E3 ---------------->
 
   <head>
