8ea050fa02c5ab5039a70d156a0438fdabc90a94
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-07-06 19:25:23 -0400

    a

--- Sites/consultas/consulta_uno_api.php
+++ Sites/consultas/consulta_uno_api.php
@@ -22,7 +22,7 @@
     $data = CallAPI($GET, 'https://e5db.herokuapp.com/messages');
     $data = json_decode($data, true);
     $data_filtrada = array();
-    echo $data;
+    echo $data[0];
     echo gettype($user);
     foreach ($data as $message) {
       if ($message[4]==$user){
