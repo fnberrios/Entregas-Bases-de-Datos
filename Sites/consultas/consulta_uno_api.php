9f01e27e4f4a4759c38360d9d01d2bf14baac831
Montserrat Cuturrufo <mscuturrufo@uc.cl>
2020-07-06 18:55:15 -0400

    j

--- Sites/consultas/consulta_uno_api.php
+++ Sites/consultas/consulta_uno_api.php
@@ -20,6 +20,7 @@
         <td>$p[4]</td> <td>$p[5]</td></tr>";
     }
     $data = CallAPI($GET, 'https://e5db.herokuapp.com/messages');
-    echo $data;
+    echo type($data);
+
     ?>
     <?php include('../templates/footer.html'); ?>
