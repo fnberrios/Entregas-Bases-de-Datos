9bb6f82ca6a3d06d39a2a6b0c8e89c22632e2b42
Franki <fnberrios@uc.cl>
2020-07-06 20:43:26 -0400

    sdf

--- Sites/consultas/consulta_cinco_api.php
+++ Sites/consultas/consulta_cinco_api.php
@@ -14,14 +14,13 @@ include('../config/call_api.php');
   $fecha_inicio = $_POST["fecha-inicio"];
   $fecha_final =  $_POST["fecha-final"];
   $user =  $_SESSION["user_id"];
-  $data = arra('id'= $user);
+  $data = array('id'=> $user);
   $query =  CallApi($GET, 'https://e5db.herokuapp.com/messages/', $data);
   echo $query;
 
   $response = json_decode($query, true); //because of true, it's in an array
   echo 'Online: '. $response['players']['online'];
   # CONSULTAS A LA API
-
   ?>
 
   <?php include('../templates/footer.html'); ?>
