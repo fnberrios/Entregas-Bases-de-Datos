259b72ab47ecd5496358ebc4612821dd36ba8fbd
Franki <fnberrios@uc.cl>
2020-07-06 21:54:03 -0400

    a

--- Sites/consultas/consulta_cinco_api.php
+++ Sites/consultas/consulta_cinco_api.php
@@ -6,6 +6,10 @@ include('../templates/header.html');
 include('../config/call_api.php');   
 ?>
 
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+crossorigin=""/>
+
 <body>
   <?php
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
@@ -29,17 +33,28 @@ include('../config/call_api.php');
                   and ($resp['date'] > $fecha_inicio)) {
       print_r($resp);
     }
+    else{
+      unset($resp);
+    }
   }
-  #foreach($obj as $key => $value) 
-  #{
-  #echo 'Your key is: '.$key.' and the value of the key is:'.$value;
-  #}
-  
-  #foreach $response as $mensaje_json
-  #  echo 'Online: '. $response['sender'];
-  # CONSULTAS A LA API
-
-  
   ?>
+
+<div id="mapid" style="height: 300px"></div>
+
+
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+   integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+   crossorigin=""></script>
+<script>
+    var map = L.map('mapid').setView([<?php echo $lat ?>, <?php echo $long ?>], 10);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    <?php foreach($response as $resp) {
+        echo 
+        'L.marker([' . $resp["lat"] . ',' . $resp["long"] . ']).addTo(map);';
+    } ?>
 
   <?php include('../templates/footer.html'); ?>
