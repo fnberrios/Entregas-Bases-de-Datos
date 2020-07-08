080efe9c150b87eb2de3fc1eaecdb9941a24af5a
Allison Uribe <asuribe@uc.cl>
2020-07-07 20:10:53 -0400

    Nuevo push

---
+++ Sites/consultas/consulta_cuatro_api.php
@@ -0,0 +1,23 @@
+<?php
+  session_start();
+?>
+<?php include('../templates/header.html');   ?>
+<?php include('../config/call_api.php');   ?>
+
+<body>
+    <?php
+    #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+    require("../config/conexion.php");
+
+    #Tenemos el id del usuario 
+    $user = $_SESSION['user_id'];
+    $data = CallAPI($GET, 'https://e5db.herokuapp.com/messages');
+    $data = json_decode($data, true);
+
+    ?>
+
+    <h3> Que mensajes deseas ingresar
+    <form action="consulta_cuatro_api_2.php" method="post">
+    Buscar: <input type="text" value="messages_f">
+</body>
+<?php include('../templates/footer.html'); ?>
